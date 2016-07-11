cab831a5941c1bf694285356d13df0dabbcfc3c8
barnebre <barnebre@gmail.com>
2016-07-11 16:45:28 -0600

    Clean Indent

--- Web_Framework/code/myweb/webs.py
+++ Web_Framework/code/myweb/webs.py
@@ -15,14 +15,15 @@ import git
 class WebServer(object):
     @cherrypy.expose
     def index(self):
-	try:
-		return open('/home/essa/workspace/code/myweb/index.html')
-	except:
-		print("Unexpected error:", sys.exc_info()[0])
+        try:
+            hey = os.path.abspath(os.getcwd())
+            return open('/home/essa/workspace/code/myweb/index.html')
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
 
 
     def readfile(self):
-	 return performance_measurement.beforeDateafterDate()
+        return performance_measurement.beforeDateafterDate()
     def git_clone(self):
         git.Repo.clone_from("https://github.com/LandPotential/LANDPKS_TESTING.git",os.path.dirname(os.path.abspath(os.getcwd())))
     def get_files(self):
@@ -37,49 +38,49 @@ class WebServer(object):
                                       "Path":"{0}\\{1}".format(subpath,file)})
         return json.dumps(f)
     def report_html(self):
-	try:
-        	return open('/home/essa/workspace/code/myweb/output/report.html')
-	except:
-                print("Unexpected error:", sys.exc_info()[0])
+        try:
+            return open('/home/essa/workspace/code/myweb/output/report.html')
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
 
 
     
     def log_html(self):
-	try:
-        	return open('/home/essa/workspace/code/myweb/output/log.html')
-	except:
-                print("Unexpected error:", sys.exc_info()[0])
+        try:
+            return open('/home/essa/workspace/code/myweb/output/log.html')
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
 
 
 
     def chart(self):
-	try:
-		msgme = performance_measurement.beforeDateafterDate()
-        	htmlpage = """<html>
+        try:
+            msgme = performance_measurement.beforeDateafterDate()
+            htmlpage = """<html>
   			<head>
-    				<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
-   				<script type="text/javascript">
-      				google.charts.load('current', {'packages':['corechart']});
-      				google.charts.setOnLoadCallback(drawChart);
-      				function drawChart() {
-        				var data = google.visualization.arrayToDataTable([""" + msgme + """]);
-        				var options = {
-          					title: 'API Performance',
+  			<script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+   			<script type="text/javascript">
+      		google.charts.load('current', {'packages':['corechart']});
+      		google.charts.setOnLoadCallback(drawChart);
+      		function drawChart() {
+        		var data = google.visualization.arrayToDataTable([""" + msgme + """]);
+        		var options = {
+          			title: 'API Performance',
           					curveType: 'function',
           					legend: { position: 'bottom' }
-        				};
-        				var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+          					};
+      					var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
         				chart.draw(data, options);
       				}
     				</script>
  			 </head>
   			<body>
-    				<div id="curve_chart" style="width: 900px; height: 500px"></div>
- 			 </body>
-		</html>"""
-	        return htmlpage
-	except:
-                print("Unexpected error:", sys.exc_info()[0])
+    		<div id="curve_chart" style="width: 900px; height: 500px"></div>
+ 			</body>
+		    </html>"""
+            return htmlpage
+        except:
+            print("Unexpected error:", sys.exc_info()[0])
 
     #public /log
     log_html.exposed = True
@@ -93,32 +94,26 @@ class WebServer(object):
     git_clone.exposed=True
 
 class WebService(object):
-     exposed = True
-
-     @cherrypy.tools.accept(media='text/plain')
-     def GET(self):
-         return cherrypy.session['mystring']
-
-
-     def POST(self, FileName, TestCase):
-         url = "sudo pybot --include {0} --outputdir /home/essa/workspace/code/myweb/output /home/essa/workspace/code/myweb/robotframework-scripts/{1}".format(TestCase,FileName)
+    exposed = True
+    @cherrypy.tools.accept(media='text/plain')
+    def GET(self):
+        return cherrypy.session['mystring']
+    def POST(self, FileName, TestCase):
+        url = "sudo pybot --include {0} --outputdir /home/essa/workspace/code/myweb/output /home/essa/workspace/code/myweb/robotframework-scripts/{1}".format(TestCase,FileName)
         # call(["pybot --outputdir output1 ", url])
-         os.system( url)
-         #shutil.copy2('/report.html', '/root/workspace/code/myweb/report.html')
-         #shutil.copy2('/log.html', '/root/workspace/code/myweb/log.html')
-         return "DONE!"
-
-     def PUT(self, another_string):
-         cherrypy.session['mystring'] = another_string
-
-
-     def DELETE(self):
-         cherrypy.session.pop('mystring', None)
+        os.system( url)
+        #shutil.copy2('/report.html', '/root/workspace/code/myweb/report.html')
+        #shutil.copy2('/log.html', '/root/workspace/code/myweb/log.html')
+        return "DONE!"
+    def PUT(self, another_string):
+        cherrypy.session['mystring'] = another_string
+    def DELETE(self):
+        cherrypy.session.pop('mystring', None)
 
 
 
 if __name__ == '__main__':
-     conf = {
+    conf = {
          '/': {
              'tools.sessions.on': True,
          },
@@ -128,9 +123,9 @@ if __name__ == '__main__':
              'tools.response_headers.headers': [('Content-Type', 'text/plain')],
          }
      }
-     webapp = WebServer()
-     webapp.luncher = WebService()
-     cherrypy.config.update({'server.socket_host': 'essa.landpotential.org',
+    webapp = WebServer()
+    webapp.luncher = WebService()
+    cherrypy.config.update({'server.socket_host': 'essa.landpotential.org',
                             'server.socket_port': 7070
                           })
-     cherrypy.quickstart(webapp, '/', conf)
+    cherrypy.quickstart(webapp, '/', conf)
