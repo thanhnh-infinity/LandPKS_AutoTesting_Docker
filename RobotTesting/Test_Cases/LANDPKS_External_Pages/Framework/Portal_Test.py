3f29ff7645148fe29fa418a67014600a26b08bd3
barnebre <barnebre@gmail.com>
2016-11-16 12:52:09 -0700

    Ujwal Setup

--- RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Portal_Test.py
+++ RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Portal_Test.py
@@ -1,1 +1,20 @@
-def Showing()
+from Test_Case import OutputErrors,OutputSucessful,SetUpApp
+from selenium.webdriver.common.by import By
+
+def Showing(self, bRobot=True):
+        global ERRORS,SUCCESS,WARNS
+        ERRORS = []
+        SUCCESS = []
+        WARNS = []
+        PassOrFail = "PASS"
+        try:
+            SetUpApp(self,bRobot=bRobot,bSelenium=True,starturl = "http://portallandpotential.businesscatalyst.com/LandPKS_FORMS/#/login",loginbutton="//a[@id='googlebutton']")
+            #########
+            # Your Stuff
+            #########
+        except:
+            PassOrFail = "PASS"
+        finally:
+            OutputErrors()
+            OutputSucessful()
+            self.tearDown(PassOrFail, bRobot,bSelenium=True)
