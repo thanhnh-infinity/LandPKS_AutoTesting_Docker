c17b3066c0c9a6728713cdfa4b1ed53b2bc2a2f0
thanhnh-infinity <thanhnh2911@gmail.com>
2016-11-18 00:35:07 -0700

    Update

--- RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
+++ RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
@@ -1427,11 +1427,15 @@ class Test_Case:#(unittest.TestCase):
                             LogSuccess("There is an layer has issues")
                          
                         if (PassOrFail == "PASS"):   
-                            LogSuccess("Test Case 10.10.2 Passed :  Review page - Submitted plot (Access from LandInfo side), check Metrics for Soil layer names, bedrock depth and stopped digging depth")
+                            LogSuccess("Test Case 10.10.2 Passed :  Review page - Submitted plot (Access from LandInfo side), check Metrics is correct for Soil layer names, bedrock depth and stopped digging depth")
                         else:
                             LogError("Test Case 10.10.2 Failed : Do not see correct Metrics in review page")
-                            
+                                
+                    except:
+                        LogError("Test Case 10.10.2 Failed : Do not see correct Metrics in review page") 
+                        PassOrFail = "FAIL"
                         
+                    try:
                         # Select Result
                         ClickElementIfVis(self.driver,By.XPATH,"//a[@href='#/landpks/landinfo_results']")
                         WaitForLoad(self.driver)
@@ -1440,14 +1444,42 @@ class Test_Case:#(unittest.TestCase):
                         graph_name = self.driver.find_elements_by_xpath("//div[@nav-view='active']//div[@class='scroll']/p[@class='lpks-p']/b[@class='ng-binding']")
                         pre_text = graph_name[0]
                         LogSuccess(pre_text.text)
+                        if (pre_text.text == 'Precipitation (mm)'):
+                            LogSuccess("--Precipitation Unit is Correct")
+                        else:
+                            LogError("--Precipitation Unit is IN-correct")
+                            PassOrFail = "FAIL"
                         temp_text = graph_name[1]
                         LogSuccess(temp_text.text)
+                        if (temp_text.text == 'Temperature (°C)'):
+                            LogSuccess("--Temperature Unit is Correct")
+                        else:
+                            LogError("--Temperature Unit is IN-correct")
+                            PassOrFail = "FAIL"
                         awc = graph_name[2]
                         LogSuccess(awc.text)
+                        if (awc.text == 'Temperature (°C)'):
+                            LogSuccess("--AWC Unit is Correct")
+                        else:
+                            LogError("--AWC Unit is IN-correct")
+                            PassOrFail = "FAIL"
+                            
+                        inside_units_name = self.driver.find_elements_by_xpath("//p[@ng-show='GLOBAL_METRIC_CONFIG === 'US_METRICS'']")
+                        elevation = inside_units_name[0]
+                        avg = inside_units_name[1]
+                        awc_2 = inside_units_name[2] 
+                        LogSuccess(elevation.text)
+                        LogSuccess(avg.text)
+                        LogSuccess(awc_2.text)
+                        
+                        if (PassOrFail == "PASS"):   
+                            LogSuccess("Test Case 10.10.3 Passed :  Result page - Submitted plot (Access from LandInfo side), check Metrics is correct for 3 Graph : Temperature, Precipitation, AWC  and 3 units : avg precipitation, awc value and elevation")
+                        else:
+                            LogError("Test Case 10.10.3 Failed : Do not see correct Metrics in Result page")
                         
                     except:
-                        LogError("Test Case 10.10.2 Failed : Do not see correct Metrics in review page") 
-                        PassOrFail = "FAIL"
+                        LogError("Test Case 10.10.3 Failed : Do not see correct Metrics in Result page") 
+                        PassOrFail = "FAIL"    
                 except:
                     PassOrFail = "FAIL"            
         except:
