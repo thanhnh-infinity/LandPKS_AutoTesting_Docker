d3e225f077c7866c19265ff58afcdeee28e82f27
thanhnh-infinity <thanhnh2911@gmail.com>
2016-11-17 23:25:45 -0700

    Update

--- RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
+++ RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
@@ -882,67 +882,49 @@ def checkMetricsAllLayers(driver, PassOrFail):
     if (layer_2 is None):
         LogSuccess("--Check name layer 2 is done - There is no Layer 2")
     else:
-        LogSuccess(layer_2.text)
+        #LogSuccess(layer_2.text)
         if (layer_2.text == '1-10 cm'):
             LogSuccess("--Layer 2 name is Correct")
-        else:
-            LogError("--Layer 2 Name is IN-correct")
-            PassOrFail = "FAIL"
             
     layer_3 = driver.find_element_by_xpath("//div[@nav-view='active']//div[@ng-show='selectedPlot.rock_fragment.soil_horizon_3 || selectedPlot.texture.soil_horizon_3']/p[@class='lpks-p']/b[@class='ng-binding']")       
     if (layer_3 is None):
         LogSuccess("--Check name layer 3 is done - There is no Layer 3")
     else:
-        LogSuccess(layer_3.text)
+        #LogSuccess(layer_3.text)
         if (layer_3.text == '10-20 cm'):
             LogSuccess("--Layer 3 name is Correct")
-        else:
-            LogError("--Layer 3 Name is IN-correct")
-            PassOrFail = "FAIL"
     
     layer_4 = driver.find_element_by_xpath("//div[@nav-view='active']//div[@ng-show='selectedPlot.rock_fragment.soil_horizon_4 || selectedPlot.texture.soil_horizon_4']/p[@class='lpks-p']/b[@class='ng-binding']")       
     if (layer_4 is None):
         LogSuccess("--Check name layer 4 is done - There is no Layer 4")
     else:
-        LogSuccess(layer_4.text)
+        #LogSuccess(layer_4.text)
         if (layer_4.text == '20-50 cm'):
             LogSuccess("--Layer 4 name is Correct")
-        else:
-            LogError("--Layer 4 Name is IN-correct")
-            PassOrFail = "FAIL"
     
     layer_5 = driver.find_element_by_xpath("//div[@nav-view='active']//div[@ng-show='selectedPlot.rock_fragment.soil_horizon_5 || selectedPlot.texture.soil_horizon_5']/p[@class='lpks-p']/b[@class='ng-binding']")       
     if (layer_5 is None):
         LogSuccess("--Check name layer 5 is done - There is no Layer 5")
     else:
-        LogSuccess(layer_5.text)
+        #LogSuccess(layer_5.text)
         if (layer_5.text == '50-70 cm'):
             LogSuccess("--Layer 5 name is Correct")
-        else:
-            LogError("--Layer 5 Name is IN-correct")
-            PassOrFail = "FAIL"
     
     layer_6 = driver.find_element_by_xpath("//div[@nav-view='active']//div[@ng-show='selectedPlot.rock_fragment.soil_horizon_6 || selectedPlot.texture.soil_horizon_6']/p[@class='lpks-p']/b[@class='ng-binding']")       
     if (layer_6 is None):
         LogSuccess("--Check name layer 6 is done - There is no Layer 6")
     else:
-        LogSuccess(layer_6.text)
+        #LogSuccess(layer_6.text)
         if (layer_6.text == '70-100 cm'):
             LogSuccess("--Layer 6 name is Correct")
-        else:
-            LogError("--Layer 6 Name is IN-correct")
-            PassOrFail = "FAIL"
             
     layer_7 = driver.find_element_by_xpath("//div[@nav-view='active']//div[@ng-show='selectedPlot.rock_fragment.soil_horizon_7 || selectedPlot.texture.soil_horizon_7']/p[@class='lpks-p']/b[@class='ng-binding']")       
     if (layer_7 is None):
         LogSuccess("--Check name layer 7 is done - There is no Layer 7")
     else:
-        LogSuccess(layer_7.text)
+        #LogSuccess(layer_7.text)
         if (layer_7.text == '100-120 cm'):
-            LogSuccess("--Layer 7 name is Correct")
-        else:
-            LogError("--Layer 7 Name is IN-correct")
-            PassOrFail = "FAIL"        
+            LogSuccess("--Layer 7 name is Correct")       
             
     return PassOrFail  
 def MapTest(driver,PassOrFail,bPortal = False ):
