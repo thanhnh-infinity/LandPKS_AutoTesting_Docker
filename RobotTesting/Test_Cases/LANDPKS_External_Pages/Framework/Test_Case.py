7ae16b555824f164eba19669e11db5779cf9f0e6
thanhnh-infinity <thanhnh2911@gmail.com>
2016-11-29 12:10:54 -0700

    Update

--- RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
+++ RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
@@ -3109,7 +3109,7 @@ class Test_Case:#(unittest.TestCase):
                         #ClickElementIfVis(self.driver,By.XPATH,"//div[@class='item item-toggle toggle-large ng-valid']/label[@class='toggle toggle-calm disable-user-behavior']")
                         #ClickElementIfVis(self.driver,By.XPATH,"//input[@ng-model='bUSorENMetric'][@type='checkbox']")
                         self.driver.find_element_by_xpath("//input[@ng-model='bUSorENMetric'][@type='checkbox']")
-                        print("Da tim thay check box")
+                        #print("Da tim thay check box")
                         #WaitForLoad(self.driver)     
                         unit_is = self.driver.find_element_by_xpath("//span[@class='ng-binding']")
                         if ('English' in unit_is):
