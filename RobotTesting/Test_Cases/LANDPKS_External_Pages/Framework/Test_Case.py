3f4d2e4a85eeb0ab22f87c991e7f1b8dd56b0a0a
thanhnh-infinity <thanhnh2911@gmail.com>
2016-11-29 12:41:04 -0700

    Update

--- RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
+++ RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
@@ -3104,23 +3104,23 @@ class Test_Case:#(unittest.TestCase):
                         print("DONOT see units toggle button")
                         PassOrFail = "FAIL"
                      
-                    try:    
+                    #try:    
                         # Click to change English Units
                         #ClickElementIfVis(self.driver,By.XPATH,"//div[@class='item item-toggle toggle-large ng-valid']/label[@class='toggle toggle-calm disable-user-behavior']")
                         #ClickElementIfVis(self.driver,By.XPATH,"//input[@ng-model='bUSorENMetric'][@type='checkbox']")
                         #self.driver.find_element_by_xpath("//input[@ng-model='bUSorENMetric'][@type='checkbox']")
                         #print("Da tim thay check box")
                         #WaitForLoad(self.driver)     
-                        unit_is = self.driver.find_element_by_xpath("//span[@class='ng-binding']")
-                        if ('English' in unit_is):
-                            LogSuccess("-- Using English Units---")
-                        else:
-                            LogSuccess("-- Using Metrics Units---")
+                        #unit_is = self.driver.find_element_by_xpath("//span[@class='ng-binding']")
+                        #if ('English' in unit_is):
+                        #    LogSuccess("-- Using English Units---")
+                        #else:
+                        #    LogSuccess("-- Using Metrics Units---")
                         #return
-                    except Exception,e:
-                        LogError(str(e))
-                        LogError("Test Case 10.10.1 Failed : CANNOT click Units Toggle Button in Application Settings") 
-                        PassOrFail = "FAIL"
+                    #except Exception,e:
+                    #    LogError(str(e))
+                    #    LogError("Test Case 10.10.1 Failed : CANNOT click Units Toggle Button in Application Settings") 
+                    #    PassOrFail = "FAIL"
                     
                     # Go back to landInfo page
                     ClickElementIfVis(self.driver,By.XPATH,"//a[@ui-sref='landpks.landpks_select_apps']")
