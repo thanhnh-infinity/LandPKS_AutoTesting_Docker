30f68a1fc5a510e3d8b54734016618d6ba1c6a25
thanhnh-infinity <thanhnh2911@gmail.com>
2016-11-29 13:54:49 -0700

    Update

--- RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
+++ RobotTesting/Test_Cases/LANDPKS_External_Pages/Framework/Test_Case.py
@@ -3108,7 +3108,7 @@ class Test_Case:#(unittest.TestCase):
                         # Click to change English Units
                         #ClickElementIfVis(self.driver,By.XPATH,"//div[@class='item item-toggle toggle-large ng-valid']/label[@class='toggle toggle-calm disable-user-behavior']")
                         #ClickElementIfVis(self.driver,By.XPATH,"//input[@ng-model='bUSorENMetric'][@type='checkbox']")
-                        self.driver.find_element_by_xpath("//input[@ng-model='bUSorENMetric'][@type='checkbox']").click()
+                        self.driver.find_element_by_xpath("//div[@class='item item-toggle toggle-large ng-valid']/div[@class='track']").click()
                         #print("Da tim thay check box")
                         #WaitForLoad(self.driver)     
                         #unit_is = self.driver.find_element_by_xpath("//span[@class='ng-binding']")
